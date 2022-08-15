719475a8b570c92a335b97ed6ae2ce089f73fbbb
Martin <martin@haslien.no>
2022-08-15 10:56:41 +0200

    Removed other views

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,5 @@ import IndexView from "@/routes/IndexView";
 import RebinderView from "@/routes/rebinder/RebinderView";
 
 export default [
-	{ path: '/', component: IndexView },
-	{ path: '/rebind', component: RebinderView },
+	{ path: '/', component: RebinderView },
 ]
